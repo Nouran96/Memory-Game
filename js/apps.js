f59120d3b40369aa1d5833a3155b8f14c9e16d90
Nouran Samy <only1nour@ymail.com>
2018-05-15 23:18:05 +0200

    Making a timer

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,19 @@
+function timerCount() {
+    sec++;
+    timer.textContent = sec;
+    return sec;
+}
+
 const startScreen = document.querySelector('.start-screen'),
-    startBtn = document.querySelector('#play');
+    startBtn = document.querySelector('#play'),
+    timerDiv = document.querySelector('.timerDiv'),
+    timer = document.querySelector('#timer');
+
+let sec = 0;
 
 startBtn.addEventListener('click', function() {
+    let interval;
     startScreen.classList.add('hidden');
+    interval = setInterval(timerCount, 1000);
 });
+
