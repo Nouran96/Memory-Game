53d7506cefa559f70fcda95dbd42ba64903f1445
Nouran Samy <only1nour@ymail.com>
2018-05-20 22:30:53 +0200

    Trying to compare the cards

--- js/apps.js
+++ js/apps.js
@@ -8,7 +8,26 @@ function timerCount() {
 }
 
 function revealCard(card) {
-    card.style.transform = 'rotateY(180deg)';
+    card.classList.toggle('transform');
+    cardsClass.push(card.classList[1]);
+    cardsShown.push(card);
+
+    if(cardsClass.length === 2){
+        compareCards();
+    }
+}
+
+function compareCards() {
+        if (cardsClass[0] === cardsClass[1]) {
+        }
+        else {
+            cardsShown.forEach(function (card) {
+                card.querySelector('.back').style.backgroundColor = 'black';
+                card.classList.toggle('transform');
+            });
+        }
+        cardsClass = [];
+        cardsShown = [];
 }
 
 // Global Declarations
@@ -18,7 +37,9 @@ const startScreen = document.querySelector('.start-screen'),
     timer = document.querySelector('#timer'),
     cards = document.querySelectorAll('.card');
 
-let sec = 0;
+let sec = 0,
+    cardsClass = [],
+    cardsShown = [];
 
 // Adding a click event for the start button to remove screen and start timer
 startBtn.addEventListener('click', function() {
@@ -27,6 +48,7 @@ startBtn.addEventListener('click', function() {
     interval = setInterval(timerCount, 1000);
 });
 
+// Adding a click event for every card to reveal its content
 for(let card of cards){
     card.addEventListener('click', function() {
         revealCard(card);
